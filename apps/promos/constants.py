ab004d23d9aae749dc4b834456a4950a37ca9afe
peterna3oum <peter-naoum@hotmail.com>
2021-03-15 00:19:47 +0200

    time validation

--- apps/promos/constants.py
+++ apps/promos/constants.py
@@ -1,6 +1,7 @@
 # Commons
 PROMO_CODE = 'promo_code'
 POINTS = 'points'
+START_TIME = 'start_time'
 END_TIME = 'end_time'
 
 # Error Messages
@@ -8,3 +9,4 @@ ERR_MSG_POINTS_POSITIVE = 'points have to be a valid positive integer.'
 ERR_MSG_ADMIN_NO_PROMO = 'Admin users are not allowed to have promos.'
 ERR_MSG_NO_ENOUGH_POINTS = 'You do not have enough points to consume in this promo.'
 ERR_MSG_PROMO_EXPIRED = 'This promo has expired.'
+ERR_MSG_END_TIME_BIGGER = 'end_time can not be smaller than start_time'
--- apps/promos/serializers/request_serializers.py
+++ apps/promos/serializers/request_serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from rest_framework.exceptions import PermissionDenied, ValidationError
 
-from apps.promos.constants import POINTS, ERR_MSG_POINTS_POSITIVE, ERR_MSG_ADMIN_NO_PROMO
+from apps.promos.constants import POINTS, ERR_MSG_POINTS_POSITIVE, ERR_MSG_ADMIN_NO_PROMO, END_TIME, START_TIME, \
+    ERR_MSG_END_TIME_BIGGER
 from apps.promos.models import Promo
 from apps.users.constants import USER, ADMIN_USER
 
@@ -15,6 +16,9 @@ class PromoRequestSerializer(serializers.ModelSerializer):
         # Assert points are positive
         if attrs[POINTS] < 0:
             raise ValidationError(ERR_MSG_POINTS_POSITIVE)
+        if attrs[END_TIME]:
+            if attrs[END_TIME] < attrs[START_TIME]:
+                raise ValidationError(ERR_MSG_END_TIME_BIGGER)
         # Reject admins to have promos
         if attrs[USER].role == ADMIN_USER:
             raise PermissionDenied(ERR_MSG_ADMIN_NO_PROMO)
